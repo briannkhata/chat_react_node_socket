bc9066d7eead196ada70f38ad61f667b578a02a9
briannkhata <briannkhata@gmail.com>
2023-03-29 22:16:10 +0200

    initital commit

--- server/index.js
+++ server/index.js
@@ -2,10 +2,28 @@ const express = require('express');
 const app = express();
 const http = require('http')
 const cors = require('cors')
+const { Server } = require('socket.io')
 
 app.use(cors());
 
 const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:5000",
+        methods:["GET","POST"]
+    }
+})
+
+
+
+io.on("connection",(socket)=>{
+    console.log(socket.id);
+
+    socket.on("disconnect",()=>{
+        console.log("Usser Disconnected");
+    })
+
+})
 
 server.listen(5000, ()=> {
     console.log("Server running at port 5000");
