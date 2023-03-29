986d5babe84fc80550e27224ad1b12069c1b925f
briannkhata <briannkhata@gmail.com>
2023-03-29 23:06:40 +0200

    house keeping

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import {useState} from "react"
+
+const socket = io.connect("http://localhost:5000");
 
 function App() {
+  const [username,setUsername] = useState("");
+  const [room,setRoom] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Join A Chat</h3>
+      <input type="text" placeholder="Brian..." />
+      <input type="text" placeholder="Room Id" />
+      <button type=""> Join A Room</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,30 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http')
-const cors = require('cors')
-const { Server } = require('socket.io')
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:5000",
-        methods:["GET","POST"]
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5000",
+    methods: ["GET", "POST"],
+  },
+});
 
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
 
-io.on("connection",(socket)=>{
-    console.log(socket.id);
-
-    socket.on("disconnect",()=>{
-        console.log("Usser Disconnected");
-    })
-
-})
-
-server.listen(5000, ()=> {
-    console.log("Server running at port 5000");
-})
+server.listen(5000, () => {
+  console.log("Server running at port 5000");
+});
